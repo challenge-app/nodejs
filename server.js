dc190220f3df0011091c3a4a900f37c25ca706d3
Mauricio Giordano <mauricio.c.giordano@gmail.com>
2014-01-07 02:35:13 -0200

    Some user stuff

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 var express = require('express'),
-    wine = require('./routes/wines');
+    user 		= require('./routes/user');
  
 var app = express();
+
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
  
 app.configure(function () {
 	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 });
  
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+app.get('/user', user.findMe);
+app.get('/user/logout', user.unAuth);
+
+app.post('/user/:id/auth', user.auth);
+
+app.get('/user/:id', user.findById);
+app.post('/user', user.addUser);
+app.put('/user/:id', user.updateUser);
+app.delete('/user/:id', user.deleteUser);
  
 app.listen(3000);
 console.log('Listening on port 3000...');
